3b0a1851ffa13a3d11dc458378213530736abb80
Matas Liutikas <matasliutikas@gmail.com>
2020-09-06 20:56:36 +0300

    moved source code to app folder

--- App.tsx
+++ App.tsx
@@ -2,12 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from './app/hooks/useCachedResources';
+import useColorScheme from './app/hooks/useColorScheme';
+import Navigation from './app/navigation';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-import WelcomeScreen from './screens/WelcomeScreen';
-import BrowseItemsScreen from './screens/BrowseItemsScreen';
+import WelcomeScreen from './app/screens/WelcomeScreen';
+import BrowseItemsScreen from './app/screens/BrowseItemsScreen';
 // import { View } from './components/Themed';
 
 export default function App() {
--- app/screens/BrowseItemsScreen.tsx
+++ app/screens/BrowseItemsScreen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, StyleSheet, Image, SafeAreaView } from "react-native";
+import { View, StyleSheet, Image, SafeAreaView, Text } from "react-native";
 
 
 function BrowseItemsScreen() {
