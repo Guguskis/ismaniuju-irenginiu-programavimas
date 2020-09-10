35a3d7aaa5461d10c2efbfb58b4161351c9aba6d
Matas Liutikas <matasliutikas@gmail.com>
2020-09-10 08:08:19 +0300

    not fully working data fetching from localhost

--- app/screens/WelcomeScreen.tsx
+++ app/screens/WelcomeScreen.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, View, Image, ImageBackground, Text } from 'react-native';
+import useAxios from 'axios-hooks'
+
 import colors from "../config/colors"
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 function WelcomeScreen() {
+    const [stock, setStock] = useState("");
+    const [{ data, loading, error }] = useAxios("http://192.168.43.170:8080/api/stock");
+
+    // setStock(data.code); // infinite loop
+
     return (
         <ImageBackground style={styles.background} source={require("../assets/images/background.jpg")} >
             <View style={styles.logoContainer}>
                 <Image style={styles.logo} source={require("../assets/images/logo-red.png")} />
                 <Text>Sell What You Don't Need</Text>
             </View>
-            <View style={styles.loginButton}></View>
+            <TouchableOpacity>
+                <View style={styles.loginButton}></View>
+            </TouchableOpacity>
             <View style={styles.registerButton}></View>
         </ImageBackground>
     );
@@ -21,7 +31,8 @@ const styles = StyleSheet.create({
         alignItems: "center",
         justifyContent: "flex-end",
         width: '100%',
-        height: '100%'
+        height: '100%',
+        paddingBottom: 50
     },
     logoContainer: {
         position: "absolute",
