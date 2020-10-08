612811618f6a5e9fc7f741461d2c687e4fe71b5f
Matas Liutikas <matasliutikas@gmail.com>
2020-10-08 08:42:46 +0300

    tried to implement sendIntent, but it's not working

--- app/university/first-test/MainScreen.tsx
+++ app/university/first-test/MainScreen.tsx
@@ -1,21 +1,39 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Linking } from 'react-native';
 import Button from './Button';
 import useNumberGenerator from './useNumberGenerator';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 
 const MainScreen = () => {
-    const [number, start, stop] = useNumberGenerator();
+    const [number, startGenerator, stopGenerator] = useNumberGenerator();
+
+    const stop = () => {
+        stopGenerator();
+
+        const action = "android.intent.action.WEB_SEARCH";
+        const extras = [
+            { "query": "dddddd" },
+            { "flag": "FLAG_ACTIVITY_NEW_TASK" }
+        ]; +
+
+            Linking.sendIntent(action, extras);
+    }
 
     return (
         <SafeAreaView>
             <Button
-                text="Start"
-                onPress={() => start(-100, 100)} />
+                text="Start -100 to 0"
+                onPress={() => startGenerator(-100, 0)} />
             <Button
                 text="Stop"
-                onPress={() => stop()} />
+                onPress={stop} />
+            <Button
+                text="Start 0 to 100"
+                onPress={() => startGenerator(0, 100)} />
+            <Button
+                text="Stop"
+                onPress={stop} />
             <Text>The number {number}</Text>
         </SafeAreaView>
     );
