e5f565d4efcfe9dc53a07601d78ec09b9af74536
Matas Liutikas <matasliutikas@gmail.com>
2020-11-25 19:49:52 +0200

    renamed university packages

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 import useCachedResources from './app/hooks/useCachedResources';
-import MainScreen from './app/university/eight-task/MainScreen';
+import MainScreen from './app/university/task-8/MainScreen';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
